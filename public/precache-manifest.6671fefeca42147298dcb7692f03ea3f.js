40388e4b90ab6f7115afbfea5b1b2a7fb369559a
JulianBrg <julian.barragan@alumno.uttehuacan.edu.mx>
2020-12-02 14:01:10 -0600

    version 1 Calendar App

---
+++ public/precache-manifest.6671fefeca42147298dcb7692f03ea3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeaf3f85af0595a77f75ed706cdf9ec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "473eff014615df7020a2",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "91c603a054ea00d86abf",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "473eff014615df7020a2",
+    "url": "/static/js/2.1de1c4ea.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.1de1c4ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c603a054ea00d86abf",
+    "url": "/static/js/main.e79fea09.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
